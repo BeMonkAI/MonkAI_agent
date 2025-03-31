0d1954bf032f58cee383917d6cd55730257266b8
José Angel Riveaux <jangel.riveaux@gmail.com>
2025-03-31 13:25:10 -0300

    correcting pyproject config

--- libs/monkai_agent.azure/src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .providers import AzureProvider, AZURE_MODELS
-
-__all__ = ['AzureProvider', 'AZURE_MODELS']
--- libs/monkai_agent/src/monkai_agent/providers.py
+++ libs/monkai_agent/src/monkai_agent/providers.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from openai import OpenAI, AzureOpenAI
 
 
 class LLMProvider:
@@ -28,3 +28,33 @@ class OpenAIProvider(LLMProvider):
             messages=messages,
             **kwargs
         )
+    
+
+# Available Azure OpenAI models
+AZURE_MODELS = [
+    'gpt-4',
+    'gpt-4-turbo',
+    'gpt-35-turbo',
+    'gpt-35-turbo-16k',
+    'gpt-4o'
+]
+
+class AzureProvider(LLMProvider):
+    """Azure OpenAI Service provider"""
+    
+    def __init__(self, api_key: str, endpoint: str, api_version: str = "2024-02-15-preview"):
+        super().__init__()
+        self.api_key = api_key
+        self.endpoint = endpoint
+        self.api_version = api_version
+    
+    def get_client(self):
+        return AzureOpenAI(
+            api_key=self.api_key,
+            api_version=self.api_version,
+            azure_endpoint=self.endpoint
+        )
+    
+    def get_completion(self, messages: list, **kwargs):
+        client = self.get_client()
+        return client.chat.completions.create(messages=messages, **kwargs)
