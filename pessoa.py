e8eaa2c9fe739b1026aca33b83366f5e85af027b
José Angel Riveaux <jangel.riveaux@gmail.com>
2025-04-08 14:52:03 -0300

    remove unnecesaries fiels

--- pessoa.py
+++
@@ -1,6 +0,0 @@
-class Pessoa:
-    def __init__(self, nome: str, idade: int, sexo: str):
-        """Inicializa a classe Pessoa com nome, idade e sexo."""
-        self.nome = nome
-        self.idade = idade
-        self.sexo = sexo
