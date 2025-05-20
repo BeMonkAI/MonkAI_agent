047192b5b29a20fe7f5bda42b2f2a35b6f501bf7
José Angel Riveaux <jangel.riveaux@gmail.com>
2025-05-20 18:53:03 -0300

    testing tools in instrumentation

--- libs/openinference_instrumentation_monkai_agent/openinference/instrumentation/monkai_agent/_wrappers.py
+++ libs/openinference_instrumentation_monkai_agent/openinference/instrumentation/monkai_agent/_wrappers.py
@@ -48,9 +48,6 @@ class _BaseProviderWrapper:
 
             tools = kwargs.get("tools", []),
             for i, tool in enumerate(tools):
-                span.set_attribute(f"ai.tools.{i}.name", tool.get("name", None))
-                span.set_attribute(f"ai.tools.{i}.description", tool.get("description", None))
-                span.set_attribute(f"ai.tools.{i}.parameters", str(tool.get("parameters", None)))
                 span.set_attribute(f"ai.tools.{i}.function", str(tool.get("function", None)))
                 span.set_attribute(f"ai.tools.{i}.tool_type", tool.get("type", ""))
 
